dea8f26dda6c83bdee45cfe043a89145ffcffa83
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-03-08 17:10:45 -0500

    Kindle-touch: Add Freescale mx308 SOC.

---
+++ modules/hardware-freescale.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkMerge mkOption types;
+  cfg = config.mobile.hardware.socs;
+in
+{
+  options.mobile.hardware.socs = {
+    freescale-MX508.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable when SOC is i.MX508";
+      internal = true;
+    };
+  };
+
+  config = {
+    mobile.system.system = mkMerge [
+      {
+        mobile = mkIf cfg.freescale-MX508.enable {
+          system.system = "armv7l-linux";
+          quirks.fb-refresher.enable = true;
+          kernel.structuredConfig = [
+            (helpers: with helpers; {
+              ARCH_MX5 = option yes;
+              ARCH_MX50 = option yes; 
+            })
+          ];
+       };
+      }
+    ];
+  };
+}
