1d19cdc8bc087ab61f4b8d6793f88bbeb43403a8
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-03-08 17:21:31 -0500

    hardware-freescale: rebuild def to get syntax correct?

--- modules/hardware-freescale.nix
+++ modules/hardware-freescale.nix
@@ -6,27 +6,33 @@ let
 in
 {
   options.mobile = {
-    hardware.socs.freescale-MX508.enable = mkOption {
+    hardware.socs.freescale-mx508.enable = mkOption {
       type = types.bool;
       default = false;
-      description = "Enable when SOC is i.MX508";
+      description = "enable when SOC is Mediatek MX508";
     };
   };
 
-  config = {
-    mobile.system.system = mkMerge [
-      {
-        mobile = mkIf cfg.freescale-MX508.enable {
-          system.system = "armv7l-linux";
-          quirks.fb-refresher.enable = true;
-          kernel.structuredConfig = [
-            (helpers: with helpers; {
-              ARCH_MX5 = option yes;
-              ARCH_MX50 = option yes; 
-            })
-          ];
-       };
-      }
-    ];
-  };
+  config = mkMerge [
+    {
+      mobile = mkIf cfg.freescale-mx508.enable {
+        system.system = "armv7l-linux";
+        quirks.fb-refresher.enable = true;
+        kernel.structuredConfig = [
+          (helpers: with helpers; {
+            ARCH_MX5 = option yes;
+            ARCH_MX50 = option yes; 
+          })
+        ];
+      };
+    }
+
+    #(mkIf anyFreescale {
+    #  mobile.kernel.structuredConfig = [
+    #    (helpers: with helpers; {
+    #      ARCH_MEDIATEK = lib.mkDefault yes;
+    #    })
+    #  ];
+    #})
+  ];
 }
