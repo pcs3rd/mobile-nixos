6095a81b5afc605b6da7492defbcbd2f2c3304b5
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-03-10 09:02:13 -0400

    pkgs.nix: bin/menuconfig nconf error not related to pinned nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,9 +1,28 @@
+#
+# “convenient” entry-point to refer to when needing a Nixpkgs.
+#
+# This is used both as a way to keep the existing code as-is,
+# but also to ensure the trace for using the pinned Nixpkgs is used.
+#
+# The pinning is now managed using `npins`.
+#
 let
-  sha256 = "sha256:0rmbq6jr21fvcspxshaiwmm5a1z66pykjm2f5imww9nhxn6dfgzv";
-  rev = "6143fc5eeb9c4f00163267708e26191d1e918932";
+  inherit (import ./npins)
+    nixpkgs
+  ;
+  channelInfo =
+    builtins.match
+      # https://releases.nixos.org/nixos/unstable/nixos-25.05beta723344.d3c42f187194/nixexprs.tar.xz
+      "https?://(.*)/([^/]+)/([^/]+)/([^/]+)/.*"
+      nixpkgs.url
+  ;
+  channelName =
+    builtins.concatStringsSep "-" [
+      (builtins.elemAt channelInfo 1)
+      (builtins.elemAt channelInfo 2)
+    ]
+  ;
+  identifier = builtins.elemAt channelInfo 3;
 in
-builtins.trace "(Using pinned Nixpkgs at ${rev})"
-import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  inherit sha256;
-})
+builtins.trace "(Using pinned Nixpkgs; ${channelName} @ ${identifier})"
+(import nixpkgs)
