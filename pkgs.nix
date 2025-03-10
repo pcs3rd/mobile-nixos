468d447f4c66834ce139ab70692fbccf67094567
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-03-09 21:59:24 -0400

    Update pkgs.nix

--- pkgs.nix
+++ pkgs.nix
@@ -1,5 +1,5 @@
 let
-  sha256 = "";
+  sha256 = sha256:0rmbq6jr21fvcspxshaiwmm5a1z66pykjm2f5imww9nhxn6dfgzv"";
   rev = "6143fc5eeb9c4f00163267708e26191d1e918932";
 in
 builtins.trace "(Using pinned Nixpkgs at ${rev})"
