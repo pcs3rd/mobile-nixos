f0305dcf00dff301769a362f39259244b83a7bbb
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-04 15:49:06 -0400

    Update default.nix

--- devices/lenovo-kodama/default.nix
+++ devices/lenovo-kodama/default.nix
@@ -21,7 +21,7 @@
   # Ensure orientation match with keyboard.
   services.udev.extraHwdb = lib.mkBefore ''
     sensor:modalias:platform:*
-      #ACCEL_MOUNT_MATRIX=0, 1, 0; -1, 0, 0; 0, 0, 1  
-      ACCEL_MOUNT_MATRIX=0, -1, 0; -1, 0, 0; 0, 0, 1
+      ACCEL_MOUNT_MATRIX=0, 1, 0; -1, 0, 0; 0, 0, 1  
+      #ACCEL_MOUNT_MATRIX=0, -1, 0; -1, 0, 0; 0, 0, 1
   '';
 }
